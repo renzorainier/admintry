7d6330177a4a42f57ce98b3064a4ceb4b01dc0a1
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-07-17 12:47:34 +0000

    Wed, Jul 17, 2024, 12:47 PM +00:00

--- src/app/Dashboard.jsx
+++ src/app/Dashboard.jsx
@@ -74,7 +74,7 @@ const Dashboard = ({ userData }) => {
                   <div
                     key={student.name}
                     className="mb-4 p-4 bg-gray-800 rounded-lg transition duration-300 hover:shadow-lg">
-                    <div className="grid grid-cols-[1fr_1fr] gap-4 items-center">
+                    <div className="grid grid-cols-[1fr_1fr] gap-1 items-center">
                       <h3 className="text-lg font-semibold truncate">{student.name}</h3>
                       <ul className="flex space-x-4 justify-end">
                         {Object.keys(student.attendance).map((date) => (
