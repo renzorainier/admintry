d0b95e005a6656beeb8360369fbb3610ad28e940
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-07-18 08:43:50 +0000

    Thu, Jul 18, 2024, 8:43 AM +00:00

--- src/app/Dashboard.jsx
+++ src/app/Dashboard.jsx
@@ -87,7 +87,7 @@ const Dashboard = ({ userData }) => {
   }, [filteredData]);
 
   return (
-    <main className="flex min-h-screen bg-[#031524] justify-center items-center">
+    <main className="flex min-h-screen bg-[#031525] justify-center items-center">
       <div className="text-white p-8 rounded-xl shadow-xl w-full max-w-3xl">
         <h1 className="text-4xl font-bold mb-8">Dashboard</h1>
         <div className="mb-8">
